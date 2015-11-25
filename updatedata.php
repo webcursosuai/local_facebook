c32f7a1192c1e9768f785450b330c1e056e5e167
Hans Cristian Jeria Díaz <hansjeria@gmail.com>
2015-11-25 15:39:29 -0300

    fix

--- updatedata.php
+++ updatedata.php
@@ -25,7 +25,7 @@
 */
 
 require_once (dirname ( dirname ( dirname ( __FILE__ ) ) ) . "/config.php");
-require_once ($CFG->dirrot."/local/facebook/app/facebook-php-sdk-master/src/facebook.php");
+include 'app/config.php';
 global $DB, $CFG;
 
 require_login ();
