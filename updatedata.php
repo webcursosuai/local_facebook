1e28265181772bbe1fb52507428598738064df2e
Hans Cristian Jeria Díaz <hansjeria@gmail.com>
2015-11-25 15:35:12 -0300

    fix link to facebook sdk

--- updatedata.php
+++ updatedata.php
@@ -25,7 +25,7 @@
 */
 
 require_once (dirname ( dirname ( dirname ( __FILE__ ) ) ) . "/config.php");
-include "facebook-php-sdk-master/src/facebook.php";
+include $CFG->dirrot."/local/facebook/app/facebook-php-sdk-master/src/facebook.php";
 global $DB, $CFG;
 
 require_login ();
