40fc6b0af01480f8ecfb8a47528b790cc98cce3c
Hans Cristian Jeria Díaz <hansjeria@gmail.com>
2016-11-30 20:21:33 -0300

    redirect to facebook

--- app/index.php
+++ app/index.php
@@ -53,8 +53,9 @@ try {
   $accessToken = $helper->getAccessToken();
 } catch(Facebook\Exceptions\FacebookResponseException $e) {
   // When Graph returns an error
-  echo 'Graph returned an error: ' . $e->getMessage();
-  exit;
+  //echo 'Graph returned an error: ' . $e->getMessage();
+  //exit;
+  redirect($CFG->fbk_url);
 } catch(Facebook\Exceptions\FacebookSDKException $e) {
   // When validation fails or other local issues
   echo 'Facebook SDK returned an error: ' . $e->getMessage();
